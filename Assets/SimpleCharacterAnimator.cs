918e9fcb7697d3947127f4da9902143456dadd77
Terje Ballestad <terj.ba@gmail.com>
2018-08-05 12:52:44 +0200

    Desk shot

---
+++ Assets/SimpleCharacterAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharacterAnimator : MonoBehaviour 
+{
+
+	public bool static_b;
+	public float speed;
+	public int AnimationNumber;
+	public int WeaponNumber;
+	private Animator animator;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () 
+	{
+		animator.SetInteger( "Animation_int", AnimationNumber);
+		animator.SetInteger("WeaponType_int", WeaponNumber);
+		animator.SetBool("Static_b", static_b);
+		animator.SetFloat("Speed_f", speed);
+
+	}
+}
